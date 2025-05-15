bc499da290425e5af5e454a8135b3161bf56cd9d
eujehniffers <jehniffers.veleiros@gmail.com>
2025-05-15 16:22:30 -0300

    Update js

--- js/script.js
+++ js/script.js
@@ -77,4 +77,31 @@
          }
      })
   }
+
+  //Isotope - Biblioteca para Filtro
+  window.addEventListener('load', () => {
+    let portfolioContainer = select(".portfolio-container");
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemsSelector: '.portfolio-item'
+      });
+
+      // Selecionar todas as listas (li) do html
+      let portfolioFilters = select('#portfolio-filters li', true);
+
+      on('click', '#portfolio-filters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+
+      }, true);
+    }
+  });
+
 })()
